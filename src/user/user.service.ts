bb48396dcf25896fadeead89956c030a2a6a47e2
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-11-24 20:39:33 +0600

    feat: implement refresh token functionality, add user profile retrieval, and enhance role-based access control

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import type { ReturnModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { UserModel } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(
+		// @ts-ignore
+		@InjectModel(UserModel)
+		private readonly userModel: ReturnModelType<typeof UserModel>
+	) {}
+
+	async byId(_id: string) {
+		const user = await this.userModel.findById(_id);
+
+		if (!user) throw new NotFoundException('User not found');
+
+		return user;
+	}
+}
