77748398bf76f4db86b1e050915ae58e72991314
Ramazan Tokoshev <r.tokoshev@gmail.com>
2025-11-25 15:51:17 +0600

    feat: implement user update functionality with validation, add ID validation pipe, and enhance user service methods

---
+++ src/pipes/id.validation.pipe.ts
@@ -0,0 +1,12 @@
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { Types } from 'mongoose';
+
+export class IdValidationPipe implements PipeTransform {
+	transform(value: string, meta: ArgumentMetadata) {
+		if (meta.type !== 'param') return value;
+
+		if (!Types.ObjectId.isValid(value)) throw new Error('Invalid id');
+
+		return value;
+	}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException,
+} from '@nestjs/common';
 import type { ReturnModelType } from '@typegoose/typegoose/lib/types';
+import { genSalt } from 'bcryptjs';
+import { hash } from 'crypto';
+import { Types } from 'mongoose';
 import { InjectModel } from 'nestjs-typegoose';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UserModel } from './user.model';
 
 @Injectable()
@@ -17,5 +26,78 @@ export class UserService {
 		if (!user) throw new NotFoundException('User not found');
 
 		return user;
+	}
+
+	async updateProfile(_id: string, dto: UpdateUserDto) {
+		const user = await this.byId(_id);
+		const isSameUser = await this.userModel.findOne({ email: dto.email });
+
+		if (isSameUser && String(_id) !== String(isSameUser._id)) {
+			throw new NotFoundException('User already exists');
+		}
+
+		if (dto.password) {
+			const salt = await genSalt(10);
+			user.password = hash(dto.password, salt);
+		}
+
+		user.email = dto.email;
+
+		if (dto.isAdmin || dto.isAdmin === false) {
+			user.isAdmin = dto.isAdmin;
+		}
+
+		await user.save();
+
+		return;
+	}
+
+	async getCount() {
+		return this.userModel.countDocuments().exec();
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {};
+
+		if (searchTerm) {
+			options = {
+				$or: [{ email: new RegExp(searchTerm, 'i') }],
+			};
+		}
+
+		return this.userModel
+			.find(options)
+			.select('-password -updatedAt -__v')
+			.sort({
+				createdAt: 'desc',
+			})
+			.exec();
+	}
+
+	// TODO: Добавил валидацию и улучшил логику
+	async delete(id: string) {
+		try {
+			if (!Types.ObjectId.isValid(id)) {
+				throw new BadRequestException('Invalid id');
+			}
+
+			const user = await this.userModel.findById(id);
+
+			if (!user) {
+				return {
+					success: false,
+					message: 'User not found',
+				};
+			}
+
+			await this.userModel.findByIdAndDelete(id);
+
+			return {
+				success: true,
+				message: 'User deleted successfully',
+			};
+		} catch (error) {
+			throw new InternalServerErrorException('Something went wrong');
+		}
 	}
 }
